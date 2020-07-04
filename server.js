08cc2c5cb54d0439aa43e83f8412a07d25b6d647
ajc133 <16407457+ajc133@users.noreply.github.com>
2020-07-04 04:15:18 +0000

    Add user create route

--- server.js
+++ server.js
@@ -17,6 +17,22 @@ app.listen(port, ()=>{
 // CREATE
 app.post('/users',(req,res)=>{
   // User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password,
+    },
+    (err,data)=>{
+      if (err){
+        res.json({success: false, message, err})
+      } else if (!data){
+        res.json({success: false, message, err})
+      } else {
+        res.json({success: true, data: data})
+      }
+    }
+  )
 })
 
 app.route('/users/:id')
